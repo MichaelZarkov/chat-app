4726b662a6850d6f10cee5e64a2129de8c7b8238
Michael Zarkov <michael.zarkov@gmail.com>
2025-05-01 17:22:18 +0300

    Finished version.

--- app/app.js
+++ app/app.js
@@ -1,32 +1,81 @@
 const socket = io();
 
-// Give random ID to the sender.
-const myId = Math.random().toString(36).substring(2, 8);
-
-// Listen for 'chat-message' event.
-socket.on('chat-message', function ({ sender, text, timestamp }) {
-  // Message text.
+// Add message to the html page.
+function addMessage(text, timestamp, isMyMessage, userID) {
   const messageText = document.createElement('p');
+  const messageTime = document.createElement('p');
+  const message = document.createElement('div');  // Wraps the text and time.
+  const userMessage = document.createElement('li');  // Wraps message and user icon.
+
   messageText.classList.add('message-text');
   messageText.textContent = text;
-  
-  // Message timestamp.
-  const messageTime = document.createElement('p');
+
   messageTime.classList.add('message-time');
-  if (sender === myId) messageTime.classList.add('my-message-time');
   messageTime.textContent = new Date(timestamp).toLocaleTimeString();
 
-  // Wrap the text and timestamp.
-  const message = document.createElement('li');
   message.classList.add('message');
-  if (sender === myId) message.classList.add('my-message');
   message.appendChild(messageText);
   message.appendChild(messageTime);
 
-  // Put the message on the html page.
+  userMessage.classList.add('user-message');
+  userMessage.appendChild(message);
+
+  if (isMyMessage) {
+    messageTime.classList.add('my-message-time');
+    message.classList.add('my-message');
+  } else {
+    // Put user icon.
+    const userIcon = document.createElement('p');
+    userIcon.classList.add('user-icon');
+    userIcon.textContent = userID;
+    userMessage.prepend(userIcon);
+  }
+
+  // Put it in the html page.
   const messages = document.getElementById('messages');
-  messages.appendChild(message);
+  messages.appendChild(userMessage);
   messages.scrollTop = messages.scrollHeight;  // Scroll to the last received message.
+}
+
+function removeAllButFirstChild(elem) {
+  while (elem.children[1]) {
+    elem.removeChild(elem.children[1])
+  }
+}
+
+// Listen for chat messages from other users.
+socket.on('chat-message', (userID, text, timestamp) => {
+  addMessage(text, timestamp, false, userID);
+});
+
+// Receives an array of user IDs which are online and updates the page.
+socket.on('users-online', userIDs => {
+  removeAllButFirstChild(document.getElementById('online-bar'));
+
+  // Add to the page.
+  userIDs.forEach((id) => {
+    const userIcon = document.createElement('p');
+    userIcon.classList.add('user-icon', 'user-icon-online');
+    userIcon.textContent = id;
+
+    document.getElementById('online-bar').appendChild(userIcon);
+  });
+
+});
+
+// A user is typing.
+let typingTimeout;
+socket.on('typing', userID => {
+  const tb = document.getElementById('typing-bar');
+  if (tb.children[0]) tb.removeChild(tb.children[0]);
+
+  const p = document.createElement('p');
+  p.classList.add('typing-text');
+  p.textContent = `${userID} is typing...`;
+  tb.appendChild(p);
+
+  clearTimeout(typingTimeout);
+  typingTimeout = setTimeout(() =>  { if (tb.children[0]) tb.removeChild(tb.children[0]); }, 2000);
 });
 
 // Send a message.
@@ -35,8 +84,13 @@ document.getElementById('form').addEventListener('submit', function (e) {
   const input = document.getElementById('input');
   const text = input.value.trim();
   if (text) {
-    const message = { text, sender: myId };
-    socket.emit('chat-message', message);
+    socket.emit('chat-message', text);
+    addMessage(text, new Date().toISOString(), true);
     input.value = '';
   }
+});
+
+// Tell the server the user is typing.
+document.getElementById('form').addEventListener('input', () => {
+  socket.emit('typing');
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  // Load environment variables from '.env' file to the 'process.env' object so we can access them.
+
 const express = require('express');
 const http = require('node:http');
 const socketIO = require('socket.io');
@@ -6,36 +8,60 @@ const path = require('node:path');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const PORT = process.env.PORT || 3000;
+
+// Keep track of connected users.
+let nextUserID = 0;
+const users = [];
 
 // Serve static files from the 'app' folder.
 app.use(express.static(path.join(__dirname, '..', 'app')));
 
 // Start the server.
-const PORT = process.env.PORT || 3000;  // Should make an 'env' file.
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
 
+function saveNewUser(socket) {
+  ++nextUserID;
+  users.push({ userID: nextUserID, socketID: socket.id });
+}
+
+function printUser(user) {
+  console.log(`User ID: ${user.userID} | Socket ID: ${user.socketID}`);
+}
+
 // Listen for WebSocket 'connection' event.
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  saveNewUser(socket);
 
-  // Listen for 'chat-message' event.
-  /*
-    Note that 'chat-message' is the name of the event so for things to work, the client has to emit with name
-    'chat-message' like so: socket.emit('chat-message', clientMessage)
-  */
-  socket.on('chat-message', ({ sender, text }) => {
-    const message = {
-      sender,
-      text, 
-      timestamp: new Date().toISOString()
-    };
-    io.emit('chat-message', message);  // Emit to all connected sockets.
+  process.stdout.write('A user connected | ');  // Don't print new line at the end.
+  printUser(users[users.length - 1]);
+
+  // Send users online list.
+  io.emit('users-online', users.map(({ userID }) => userID));
+
+  socket.on('chat-message', text => {
+    const { userID } = users.find(({ socketID }) => socketID === socket.id);
+    const timestamp = new Date().toISOString();
+    socket.broadcast.emit('chat-message', userID, text, timestamp);  // Emit the message to all other users.
   });  
 
-  // Listen for 'disconnect' event.
+  // The user is typing.
+  socket.on('typing', () => {
+    const { userID } = users.find(({ socketID }) => socketID === socket.id);
+    socket.broadcast.emit('typing', userID);
+  });
+
+  /*
+  Note: If you open a browser tab and reload the page very fast a couple of times, the users online array might have some users which are disconnected but are still in it.
+  They resolve after about 30 seconds. 'Socket.IO' might have some disconnect timeout so maybe that's why the disconnect is not instantaneous (or there may be another reason idk?).
+  */
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    const i = users.findIndex(({ socketID }) => socketID === socket.id);  // Find disconnected user.
+    process.stdout.write('A user disconnected | ');  // Don't print new line at the end.
+    printUser(users[i]);
+    users.splice(i, 1);  // Remove user from online array.
+    io.emit('users-online', users.map(({ userID }) => userID));
   });
 });
